c1ca55c6a6e2f4efb4684d122edfbb1961455f2c
Keks <keks@htmlacademy.ru>
2023-03-20 12:41:16 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/validation-form.js
+++ 8/js/validation-form.js
@@ -4,62 +4,33 @@ const hashtag = document.querySelector('.text__hashtags');
 const REGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASHTAG = 5;
 
-const messageHashtagError = `Нельзя указать больше ${MAX_HASHTAG} хэш-тегов`;
-
 const pristine = new Pristine(imgUploadForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
   errorTextTag: 'span',
-  errorTextClass: 'form__error'
-}, false);
+  errorTextClass: 'img-upload__error'
+});
 
 const checkIsHashtagRegexp = (hashtags) => {
-  for (let i = 0; i < hashtags.length; i++) {
-    if (REGEXP.test(hashtags[i])) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const checkHashtagSame = (hashtags) => {
-  for (let i = 0; i < hashtags.length; i++) {
-    const currentItem = hashtags[i].toLowerCase();
-    for (let j = i + 1; j < hashtags.length; j++) {
-      const nextItem = hashtags[j].toLowerCase();
-      if (currentItem === nextItem) {
-        return true;
-      }
-    }
-  }
-  return false;
+  const hashtagArray = hashtags.trim().split(' ');
+  const isValid = hashtagArray.some((hashtagElement) => !REGEXP.test(hashtagElement));
+  return (!hashtag.value.length) ? isValid : !isValid;
 };
 
 const checkHashtagLength = (hashtags) => {
-  if (hashtags.length > MAX_HASHTAG) {
-    return true;
-  }
-  return false;
+  const hashtagArray = hashtags.trim().split(' ');
+  return hashtagArray.length <= MAX_HASHTAG;
 };
 
-
-const validateHashtag = () => {
-  const hashtags = hashtag.value.trim().split(' ');
-  const isValid = checkIsHashtagRegexp(hashtags) && checkHashtagSame(hashtags) && checkHashtagLength(hashtags);
-  if (hashtag.length > 0) {
-    return isValid;
-  }
-  return false;
+const checkHashtagSame = (hashtags) => {
+  const hashtagArray = hashtags.trim().split(' ');
+  return new Set(hashtagArray).size === hashtagArray.length;
 };
 
+pristine.addValidator(hashtag, checkIsHashtagRegexp, 'неверный хэш-тег, хэш-теги должны разделяться пробелами');
+pristine.addValidator(hashtag, checkHashtagLength, `нельзя указать больше ${MAX_HASHTAG} хэш-тегов`);
+pristine.addValidator(hashtag, checkHashtagSame, 'один и тот же хэш-тег не может быть использован дважды');
 
-pristine.addValidator(hashtag, validateHashtag, messageHashtagError);
-
-const validate = () => {
-  if (pristine.validate()) {
-    return true;
-  }
-  return false;
-};
+const validate = () => !pristine.validate();
 
 export { validate };
