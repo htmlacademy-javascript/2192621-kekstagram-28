479bf8972ac72286c4b9e426777210533f3c7f2b
Artur Muzafarov <110480339+ArtMuza@users.noreply.github.com>
2023-03-31 18:14:28 +0500

    Фото на память

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { loadPhoto } from './uploading-image.js';
 import { getData } from './api.js';
 import { showAlert } from './util.js';
 import { initFilter } from './filter.js';
+import './avatar.js';
 
 getData()
   .then((pictures) => {
